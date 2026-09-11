e9fe4b79db2618ffe12d9129596a6ce4cbde315e
khromo1 <tynybekovtemirlan7@gmail.com>
2026-09-11 15:03:01 +0500

    director

--- src/playlist/PlaylistDirector.java
+++ src/playlist/PlaylistDirector.java
@@ -1,4 +1,19 @@
 package playlist;
-
+import java.util.List;
 public class PlaylistDirector {
+    public Playlist createDailyMix(List<String> tracks) {
+        PlaylistBuilder builder = new PlaylistBuilder()
+                .setName("DailyMix")
+                .setShuffle(true);
+        tracks.forEach(builder::addTrack);
+        return builder.build();
+    }
+    public Playlist createFocusPlaylist(List<String> tracks) {
+        PlaylistBuilder builder = new PlaylistBuilder()
+                .setName("Deep Focus")
+                .setRepeat(true)
+                .setPrivate(true);
+        tracks.forEach(builder::addTrack);
+        return builder.build();
+    }
 }
